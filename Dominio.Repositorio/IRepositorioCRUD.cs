a777e3dbf163bdb4020b8c63a2ffaeb3b72d4bac
FernandoJulca <ferjose222810@gmail.com>
2025-04-25 18:32:29 -0500

    agregar .gitignore y limpiar archivos temporales

---
+++ Dominio.Repositorio/IRepositorioCRUD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Repositorio
+{
+    public interface IRpositorioCRUD<T> where T : class
+    {
+        string Agregar(T reg);
+        string Actualizar(T reg);
+
+        T Buscar(string id);
+    }
+}
