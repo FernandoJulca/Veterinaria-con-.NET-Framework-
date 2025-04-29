677e40d48635f289e37116355fb1a82d63f4ebfb
FernandoJulca <ferjose222810@gmail.com>
2025-04-29 14:01:50 -0500

    añadido del EDIT, mejorando la distribucion de la arquitectura en capas

---
+++ Aplicacion.Servicios/GestionCategoria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Dominio.Entidad.Entidad;
+using Infraestructura.Data;
+
+namespace Aplicacion.Servicios
+{
+    public class GestionCategoria
+    {
+        private readonly CategoriaDTO _categoria;
+
+        public GestionCategoria()
+        {
+            _categoria = new CategoriaDTO();
+        }
+
+        public async Task<SelectList> ListarCategoria()
+        {
+            IEnumerable<Categoria> categorias = await _categoria.Listar();
+            return new SelectList(categorias, "IdCategoria", "NombreCategoria");
+        }
+
+        public async Task<SelectList> ListarCategoriaPost(Producto reg)
+        {
+            IEnumerable<Categoria> categorias = await _categoria.Listar();
+            return new SelectList(categorias, "IdCategoria", "NombreCategoria",reg.IdCategoria);
+        }
+    }
+}
---
+++ Infraestructura.Data/EstadoDTO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Entidad.Abstraccion;
+using Dominio.Entidad.Entidad;
+
+namespace Infraestructura.Data
+{
+    public class EstadoDTO : IEstado
+    {
+        public async Task<IEnumerable<Estado>> Listar()
+        {
+            List<Estado> estados = new List<Estado>();
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["cadena"].ConnectionString))
+                {
+                    await cnn.OpenAsync();
+                    SqlCommand cmd = new SqlCommand("usp_listar_estado", cnn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+
+                            estados.Add(new Estado()
+                            {
+                                IdEstado = dr.GetInt32(0),
+                                Descripcion = dr.GetString(1)
+                            });
+                        }
+                    }
+                }
+
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error en la base de datos", ex);
+
+            }
+            catch (Exception ex) {
+                throw new Exception("Error al listar estado", ex);
+            
+            
+            }
+           return estados;
+        }
+    }
+}
