b8b69db213cb3ca44483b2253469e604e8cfdd1b
FernandoJulca <ferjose222810@gmail.com>
2025-05-03 15:13:01 -0500

    Agregando un diseño basico y agregando tablas a la base de datos

--- ProyectoVeterinaria/Controllers/HomeController.cs
+++ ProyectoVeterinaria/Controllers/HomeController.cs
@@ -13,14 +13,14 @@ namespace ProyectoVeterinaria.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Nosotros()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Sedes()
         {
             ViewBag.Message = "Your contact page.";
 
